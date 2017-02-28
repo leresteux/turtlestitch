6f3ff07c5eed6b616bf9d5d60613d1023fbac9e8
Michael Aschauer <m@ash.to>
2017-02-28 23:09:39 +0100

    fix mouse sensing

--- stitchcode/threads.js
+++ stitchcode/threads.js
@@ -6,8 +6,7 @@ Process.prototype.reportMouseX = function () {
             world = stage.world();
             if (world) {
 				var factor = stage.renderer.isParallelProjection ? 65 / stage.camera.zoomFactor : stage.controls.object.position.length() / 10;
-                return ((world.hand.position().x - stage.center().x)
-                    / stage.scale) / stage.camera.zoomFactor  * 2 + stage.controls.center.x;
+                return ((world.hand.position().x - stage.center().x)) / stage.camera.zoomFactor  * 2 + stage.controls.center.x;
             }
         }
     }
@@ -22,8 +21,7 @@ Process.prototype.reportMouseY = function () {
             world = stage.world();
             if (world) {
 				var factor = stage.renderer.isParallelProjection ? 65 / stage.camera.zoomFactor : stage.controls.object.position.length() / 10;
-                return ((stage.center().y - world.hand.position().y)
-                    / stage.scale) / stage.camera.zoomFactor * 2 + stage.controls.center.y;
+                return ((stage.center().y - world.hand.position().y)) / stage.camera.zoomFactor * 2 + stage.controls.center.y;
             }
         }
     }
