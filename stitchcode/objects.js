32ba70b59ce5df232744f04ca1016b7a5b68772f
Simon-Mong <30740101+Simon-Mong@users.noreply.github.com>
2021-09-08 19:23:27 +0800

    replace 'glide' with 'reportRandomPosition'
    replace 'glide' with 'reportRandomPosition'

--- stitchcode/objects.js
+++ stitchcode/objects.js
@@ -1461,7 +1461,14 @@ SpriteMorph.prototype.initBlocks = function () {
         spec: 'text length of %s with size %n',
         defaults: ["hello", 21]
     };
-
+    this.blocks.reportRandomPosition =
+    {
+		    only: SpriteMorph,
+        type: 'reporter',
+        category: 'motion',
+        spec: 'random position',
+    };
+	
     // pen blocks
 
     this.blocks.isPenDown =
@@ -1818,7 +1825,7 @@ SpriteMorph.prototype.blockTemplates = function (category) {
         //blocks.push(block('gotoXYIn'));
         //blocks.push(block('gotoXYBy'));
         blocks.push(block('doGotoObject'));
-        blocks.push(block('doGlide'));
+        blocks.push(block('reportRandomPosition'));
         blocks.push('-');
         blocks.push(block('changeXPosition'));
         blocks.push(block('setXPosition'));
